55818179f38e459a67d330d18e51a1abfdd4178e
Roman Tsisyk <roman@tsisyk.com>
2018-10-04 12:01:05 +0300

    Implement basic command line argument handling
    Closes #13

--- src/stegos.rs
+++ src/stegos.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::new("Stegos")
+        .version(crate_version!())
+        .author("Stegos AG <info@stegos.cc>")
+        .about("Stegos is a completely anonymous and confidential cryptocurrency.")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Path to stegos.toml configuration file")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let config = args.value_of("config").unwrap_or("stegos.toml");
+    println!("Configuration file: {}", config);
 }

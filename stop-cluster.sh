58553f0990c9cfb7f8124d15c16e014e4ba77ca4
Eugene Chupriyanov <tchu@tchu.ru>
2018-11-16 15:20:16 +0300

    Added scripts to start/stop 3-node blockchain on localhost

---
+++ stop-cluster.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+nodes_count=${1:-3}
+# Stop all the nodes
+for (( i=1 ; i<=${nodes_count} ; i++ )) ; do
+  echo Stopping Node ${i}
+  tmux kill-session -t node${i} 2>/dev/null
+done

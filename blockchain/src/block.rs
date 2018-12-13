stegos/stegos

commit 40c5ad53ac79378389436444262beee09c680256

Add unit tests for blockchain node
- Pass genesis blocks to node's constructor explicitly
- Don't include extra MONETARY_FEE for data transactions
- Add basic unit tests for NodeService
- Allow to specify the total number of coins in bootstrap utility
- Use usize instead of u64 for DATA_UNIT
- Improve debug output in Broker
- Remove NodeService::outbox (an artifact)

See #190

blockchain/src/block.rs
@@ -327,7 +327,7 @@ impl Hashable for MonetaryBlock {
 }
 
 /// Types of blocks supported by this blockchain.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Block {
     KeyBlock(KeyBlock),
     MonetaryBlock(MonetaryBlock),

node/src/lib.rs
@@ -50,6 +50,22 @@ use tokio_timer::Interval;
 // Public API.
 // ----------------------------------------------------------------
 
+/// Load genesis blocks for tests and development.
+pub fn genesis_dev() -> Result<Vec<Block>, Error> {
+    // Load generated blocks
+    let key_block = include_bytes!("../data/genesis0.bin");
+    let key_block: protos::node::KeyBlock = protobuf::parse_from_bytes(&key_block[..])?;
+    let key_block = KeyBlock::from_proto(&key_block)?;
+    let monetary_block = include_bytes!("../data/genesis1.bin");
+    let monetary_block: protos::node::MonetaryBlock =
+        protobuf::parse_from_bytes(&monetary_block[..])?;
+    let monetary_block = MonetaryBlock::from_proto(&monetary_block)?;
+    Ok(vec![
+        Block::KeyBlock(key_block),
+        Block::MonetaryBlock(monetary_block),
+    ])
+}
+
 /// Blockchain Node.
 #[derive(Clone, Debug)]
 pub struct Node {
@@ -60,11 +76,13 @@ impl Node {
     /// Create a new blockchain node.
     pub fn new(
         keys: KeyChain,
+        genesis: Vec<Block>,
         broker: Broker,
     ) -> Result<(impl Future<Item = (), Error = ()>, Node), Error> {
         let (outbox, inbox) = unbounded();
 
-        outbox.unbounded_send(NodeMessage::Init)?;
+        let msg = NodeMessage::Init { genesis };
+        outbox.unbounded_send(msg)?;
 
         let service = NodeService::new(keys, broker, inbox)?;
         let handler = Node { outbox };
@@ -133,12 +151,20 @@ pub struct MessageNotification {
 // Internal Implementation.
 // ----------------------------------------------------------------
 
+/// Blockchain version.
 const VERSION: u64 = 1;
+/// Mempool processing interval.
+// TODO: replace with randound rounds
 const MEMPOOL_TTL: u64 = 15;
+/// Topic used for sending transactions.
 const TX_TOPIC: &'static str = "tx";
+/// Topic used for sending sealed blocks.
 const BLOCK_TOPIC: &'static str = "block";
+/// Fixed fee for monetary transactions.
 const MONETARY_FEE: i64 = 1;
-const DATA_UNIT: u64 = 1024;
+/// Data unit used to calculate fee.
+const DATA_UNIT: usize = 1024;
+/// Fee for one DATA_UNIT.
 const DATA_UNIT_FEE: i64 = 1;
 
 #[derive(Clone, Debug)]
@@ -168,11 +194,13 @@ enum NodeMessage {
     //
     // Internal Events
     //
-    Init,
+    Init {
+        genesis: Vec<Block>,
+    },
     Timer(Instant), // TODO: replace with RandHound
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Fail, PartialEq, Eq)]
 pub enum NodeError {
     #[fail(display = "Amount should be greater than zero.")]
     ZeroOrNegativeAmount,
@@ -280,37 +308,33 @@ impl NodeService {
     }
 
     /// Handler for NodeMessage::Init.
-    fn handle_init(&mut self) -> Result<(), Error> {
+    fn handle_init(&mut self, genesis: Vec<Block>) -> Result<(), Error> {
         info!("Registering genesis blocks...");
-
-        // Load generated blocks
-        let key_block = include_bytes!("../data/genesis0.bin");
-        let key_block: protos::node::KeyBlock = protobuf::parse_from_bytes(&key_block[..])?;
-        let key_block = KeyBlock::from_proto(&key_block)?;
-
-        let monetary_block = include_bytes!("../data/genesis1.bin");
-        let monetary_block: protos::node::MonetaryBlock =
-            protobuf::parse_from_bytes(&monetary_block[..])?;
-        let monetary_block = MonetaryBlock::from_proto(&monetary_block)?;
-
-        info!("Genesis key block: hash={}", Hash::digest(&key_block));
-        info!(
-            "Genesis monetary block: hash={}",
-            Hash::digest(&monetary_block)
-        );
 
         //
         // Sic: genesis block has invalid monetary balance, so handle_monetary_block_request()
         // can't be used here.
         //
 
-        let key_block2 = key_block.clone();
-        self.chain.register_key_block(key_block)?;
-        self.on_key_block_registered(&key_block2);
-
-        let monetary_block2 = monetary_block.clone();
-        let inputs = self.chain.register_monetary_block(monetary_block)?;
-        self.on_monetary_block_registered(&monetary_block2, &inputs);
+        for block in genesis {
+            match block {
+                Block::KeyBlock(key_block) => {
+                    info!("Genesis key block: hash={}", Hash::digest(&key_block));
+                    let key_block2 = key_block.clone();
+                    self.chain.register_key_block(key_block)?;
+                    self.on_key_block_registered(&key_block2);
+                }
+                Block::MonetaryBlock(monetary_block) => {
+                    info!(
+                        "Genesis monetary block: hash={}",
+                        Hash::digest(&monetary_block)
+                    );
+                    let monetary_block2 = monetary_block.clone();
+                    let inputs = self.chain.register_monetary_block(monetary_block)?;
+                    self.on_monetary_block_registered(&monetary_block2, &inputs);
+                }
+            }
+        }
 
         Ok(())
     }
@@ -631,9 +655,9 @@ impl NodeService {
     }
 
     /// Calculate fee for data transaction.
-    fn data_fee(size: u64, ttl: u64) -> i64 {
+    fn data_fee(size: usize, ttl: u64) -> i64 {
         assert!(size > 0);
-        let units: u64 = (size + (DATA_UNIT - 1u64)) / DATA_UNIT;
+        let units: usize = (size + (DATA_UNIT - 1)) / DATA_UNIT;
         (units as i64) * (ttl as i64) * DATA_UNIT_FEE
     }
 
@@ -785,24 +809,22 @@ impl NodeService {
         // Find inputs
         //
 
-        let fee = NodeService::data_fee(data.len() as u64, ttl);
+        let fee = NodeService::data_fee(data.len(), ttl);
         // Try to find exact sum plus fee, without a change.
-        let (fee, change, inputs) =
-            match NodeService::find_utxo_exact(&self.unspent, fee + MONETARY_FEE) {
-                Some(inputs) => {
-                    // If found, then charge the minimal fee.
-                    let fee = fee + MONETARY_FEE;
-                    let inputs = self.chain.outputs_by_hashes(&[inputs])?;
-                    (fee, 0i64, inputs)
-                }
-                None => {
-                    // Otherwise, charge the double fee.
-                    let fee = fee + 2 * MONETARY_FEE;
-                    let (inputs, change) = NodeService::find_utxo(&self.unspent, fee)?;
-                    let inputs = self.chain.outputs_by_hashes(&inputs)?;
-                    (fee, change, inputs)
-                }
-            };
+        let (fee, change, inputs) = match NodeService::find_utxo_exact(&self.unspent, fee) {
+            Some(inputs) => {
+                // If found, then charge the minimal fee.
+                let inputs = self.chain.outputs_by_hashes(&[inputs])?;
+                (fee, 0i64, inputs)
+            }
+            None => {
+                // Otherwise, charge the double fee.
+                let fee = fee + MONETARY_FEE;
+                let (inputs, change) = NodeService::find_utxo(&self.unspent, fee)?;
+                let inputs = self.chain.outputs_by_hashes(&inputs)?;
+                (fee, change, inputs)
+            }
+        };
 
         info!(
             "Transaction preview: recipient={}, ttl={}, spent={}, change={}, fee={}",
@@ -958,7 +980,7 @@ impl Future for NodeService {
             match self.events.poll().expect("all errors are already handled") {
                 Async::Ready(Some(event)) => {
                     let result: Result<(), Error> = match event {
-                        NodeMessage::Init => self.handle_init(),
+                        NodeMessage::Init { genesis } => self.handle_init(genesis),
                         NodeMessage::PaymentRequest { recipient, amount } => {
                             self.handle_payment_request(&recipient, amount)
                         }
@@ -991,6 +1013,253 @@ impl Future for NodeService {
 #[cfg(test)]
 pub mod tests {
     use super::*;
+
+    #[test]
+    pub fn init() {
+        simple_logger::init_with_level(log::Level::Debug).unwrap_or_default();
+        let keys = KeyChain::new_mem();
+        let (_outbox, inbox) = unbounded();
+        let (broker_tx, _broker_rx) = unbounded();
+        let broker = Broker {
+            upstream: broker_tx,
+        };
+        let mut node = NodeService::new(keys.clone(), broker, inbox).unwrap();
+
+        assert_eq!(node.chain.blocks().len(), 0);
+        assert_eq!(node.balance, 0);
+        assert_eq!(node.unspent.len(), 0);
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.epoch, 1);
+        assert_ne!(node.leader, keys.cosi_pkey);
+        assert_eq!(node.witnesses.len(), 0);
+
+        let amount: i64 = 3_000_000;
+        let genesis = genesis(&[keys.clone()], amount);
+        let genesis_count = genesis.len();
+        node.handle_init(genesis).unwrap();
+        assert_eq!(node.chain.blocks().len(), genesis_count);
+        assert_eq!(node.balance, amount);
+        assert_eq!(node.unspent.len(), 1);
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.epoch, 2);
+        assert_eq!(node.leader, keys.cosi_pkey);
+        assert_eq!(node.witnesses.len(), 1);
+        assert_eq!(node.witnesses[0], node.leader);
+    }
+
+    #[test]
+    pub fn monetary_requests() {
+        simple_logger::init_with_level(log::Level::Debug).unwrap_or_default();
+        let keys = KeyChain::new_mem();
+        let (_outbox, inbox) = unbounded();
+        let (broker_tx, _broker_rx) = unbounded();
+        let broker = Broker {
+            upstream: broker_tx,
+        };
+        let mut node = NodeService::new(keys.clone(), broker, inbox).unwrap();
+
+        let total: i64 = 3_000_000;
+        let genesis = genesis(&[keys.clone()], total);
+        node.handle_init(genesis).unwrap();
+        let mut block_count = node.chain.blocks().len();
+
+        // Invalid requests.
+        let e = node
+            .handle_payment_request(&keys.wallet_pkey, -1)
+            .unwrap_err();
+        assert_eq!(
+            e.downcast::<NodeError>().unwrap(),
+            NodeError::ZeroOrNegativeAmount
+        );
+        let e = node
+            .handle_payment_request(&keys.wallet_pkey, 0)
+            .unwrap_err();
+        assert_eq!(
+            e.downcast::<NodeError>().unwrap(),
+            NodeError::ZeroOrNegativeAmount
+        );
+        let e = node
+            .handle_payment_request(&keys.wallet_pkey, total)
+            .unwrap_err();
+        assert_eq!(
+            e.downcast::<NodeError>().unwrap(),
+            NodeError::NotEnoughMoney
+        );
+
+        // Payment without a change.
+        node.handle_payment_request(&keys.wallet_pkey, total - MONETARY_FEE)
+            .unwrap();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.balance, total); // fee is returned back
+        assert_eq!(node.unspent.len(), 2);
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        let mut amounts = Vec::new();
+        for (unspent, _) in node.unspent.iter() {
+            match node.chain.output_by_hash(unspent) {
+                Some(Output::MonetaryOutput(o)) => {
+                    let (_, _, amount) = o.decrypt_payload(&keys.wallet_skey).unwrap();
+                    amounts.push(amount);
+                }
+                _ => panic!(),
+            }
+        }
+        amounts.sort();
+        assert_eq!(amounts, vec![MONETARY_FEE, total - MONETARY_FEE]);
+        block_count += 1;
+
+        // Payment with a change.
+        node.handle_payment_request(&keys.wallet_pkey, 100).unwrap();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.balance, total); // fee is returned back
+        assert_eq!(node.unspent.len(), 3);
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        let mut amounts = Vec::new();
+        for (unspent, _) in node.unspent.iter() {
+            match node.chain.output_by_hash(unspent) {
+                Some(Output::MonetaryOutput(o)) => {
+                    let (_, _, amount) = o.decrypt_payload(&keys.wallet_skey).unwrap();
+                    amounts.push(amount);
+                }
+                _ => panic!(),
+            }
+        }
+        amounts.sort();
+        let expected = vec![2 * MONETARY_FEE, 100, total - 100 - 2 * MONETARY_FEE];
+        assert_eq!(amounts, expected);
+
+        assert_eq!(block_count, 3);
+    }
+
+    #[test]
+    pub fn data_requests() {
+        simple_logger::init_with_level(log::Level::Debug).unwrap_or_default();
+        let keys = KeyChain::new_mem();
+        let (_outbox, inbox) = unbounded();
+        let (broker_tx, _broker_rx) = unbounded();
+        let broker = Broker {
+            upstream: broker_tx,
+        };
+        let mut node = NodeService::new(keys.clone(), broker, inbox).unwrap();
+
+        let total: i64 = 100;
+        let genesis = genesis(&[keys.clone()], total);
+        node.handle_init(genesis).unwrap();
+        let mut block_count = node.chain.blocks().len();
+
+        // Invalid requests.
+        let e = node
+            .handle_message_request(&keys.wallet_pkey, 100500, b"hello".to_vec())
+            .unwrap_err();
+        assert_eq!(
+            e.downcast::<NodeError>().unwrap(),
+            NodeError::NotEnoughMoney
+        );
+
+        let data = b"hello".to_vec();
+        let ttl = 3;
+        let data_fee = NodeService::data_fee(data.len(), ttl);
+
+        // Change money for the next test.
+        node.handle_payment_request(&keys.wallet_pkey, data_fee)
+            .unwrap();
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.balance, total); // fee is returned back
+        assert_eq!(node.unspent.len(), 3);
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        let mut amounts = Vec::new();
+        for (unspent, _) in node.unspent.iter() {
+            match node.chain.output_by_hash(unspent) {
+                Some(Output::MonetaryOutput(o)) => {
+                    let (_, _, amount) = o.decrypt_payload(&keys.wallet_skey).unwrap();
+                    amounts.push(amount);
+                }
+                _ => panic!(),
+            }
+        }
+        amounts.sort();
+        let expected = vec![
+            2 * MONETARY_FEE,
+            data_fee,
+            total - data_fee - 2 * MONETARY_FEE,
+        ];
+        assert_eq!(amounts, expected);
+        block_count += 1;
+
+        // Send data without a change.
+        node.handle_message_request(&keys.wallet_pkey, ttl, data)
+            .unwrap();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 1); // mempool contains "ack" for data
+        assert_eq!(node.balance, total); // fee is returned back
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        let mut amounts = Vec::new();
+        for (unspent, _) in node.unspent.iter() {
+            match node.chain.output_by_hash(unspent) {
+                Some(Output::MonetaryOutput(o)) => {
+                    let (_, _, amount) = o.decrypt_payload(&keys.wallet_skey).unwrap();
+                    amounts.push(amount);
+                }
+                _ => panic!(),
+            }
+        }
+        amounts.sort();
+        let expected = vec![
+            2 * MONETARY_FEE,
+            data_fee,
+            total - data_fee - 2 * MONETARY_FEE,
+        ];
+        assert_eq!(amounts, expected);
+        block_count += 1;
+
+        // Spent data transaction.
+        let unspent_len = node.chain.unspent().len();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.chain.unspent().len(), unspent_len - 1);
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        block_count += 1;
+
+        // Send data with a change.
+        let data = b"hello".to_vec();
+        let ttl = 10;
+        let data_fee2 = NodeService::data_fee(data.len(), ttl);
+        node.handle_message_request(&keys.wallet_pkey, ttl, data)
+            .unwrap();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 1); // mempool contains "ack" for data
+        assert_eq!(node.balance, total); // fee is returned back
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+        let mut amounts = Vec::new();
+        for (unspent, _) in node.unspent.iter() {
+            match node.chain.output_by_hash(unspent) {
+                Some(Output::MonetaryOutput(o)) => {
+                    let (_, _, amount) = o.decrypt_payload(&keys.wallet_skey).unwrap();
+                    amounts.push(amount);
+                }
+                _ => panic!(),
+            }
+        }
+        amounts.sort();
+        let expected = vec![MONETARY_FEE + data_fee2, total - MONETARY_FEE - data_fee2];
+        assert_eq!(amounts, expected);
+        block_count += 1;
+
+        // Spent data a transaction.
+        let unspent_len = node.chain.unspent().len();
+        assert_eq!(node.mempool.len(), 1);
+        node.process_mempool().unwrap();
+        assert_eq!(node.mempool.len(), 0);
+        assert_eq!(node.chain.unspent().len(), unspent_len - 1);
+        assert_eq!(node.chain.blocks().len(), block_count + 1);
+    }
 
     /// Check transaction signing and validation.
     #[test]

blockchain/src/blockchain.rs
@@ -328,11 +328,17 @@ pub mod tests {
             KeyChain::new_mem(),
         ];
 
-        let (key_block, monetary_block) = genesis(&keychains);
+        let blocks = genesis(&keychains, 1_000_000);
 
         let mut blockchain = Blockchain::new();
-        blockchain.register_key_block(key_block).unwrap();
-        blockchain.register_monetary_block(monetary_block).unwrap();
+        for block in blocks {
+            match block {
+                Block::KeyBlock(block) => blockchain.register_key_block(block).unwrap(),
+                Block::MonetaryBlock(block) => {
+                    blockchain.register_monetary_block(block).unwrap();
+                }
+            }
+        }
 
         assert!(blockchain.blocks().len() > 0);
         iterate(&mut blockchain).unwrap();

blockchain/src/genesis.rs
@@ -29,7 +29,9 @@ use stegos_crypto::pbc::secure as cosi_keys;
 use stegos_keychain::KeyChain;
 
 /// Genesis blocks.
-pub fn genesis(keychains: &[KeyChain]) -> (KeyBlock, MonetaryBlock) {
+pub fn genesis(keychains: &[KeyChain], amount: i64) -> Vec<Block> {
+    let mut blocks = Vec::with_capacity(2);
+
     // Both block are created at the same time in the same epoch.
     let version: u64 = 1;
     let epoch: u64 = 1;
@@ -57,7 +59,6 @@ pub fn genesis(keychains: &[KeyChain]) -> (KeyBlock, MonetaryBlock) {
     let block2 = {
         let previous = Hash::digest(&block1);
         let base = BaseBlockHeader::new(version, previous, epoch, timestamp);
-        let amount: i64 = 1_000_000;
 
         // Genesis doesn't have inputs
         let inputs = Vec::<Hash>::new();
@@ -75,5 +76,8 @@ pub fn genesis(keychains: &[KeyChain]) -> (KeyBlock, MonetaryBlock) {
         MonetaryBlock::new(base, gamma, &inputs, &outputs)
     };
 
-    (block1, block2)
+    blocks.push(Block::KeyBlock(block1));
+    blocks.push(Block::MonetaryBlock(block2));
+
+    blocks
 }

blockchain/src/output.rs
@@ -305,9 +305,9 @@ impl DataOutput {
         Ok((delta, gamma, data))
     }
 
-    pub fn data_size(&self) -> u64 {
+    pub fn data_size(&self) -> usize {
         assert!(self.payload.ctxt.len() > DATA_PAYLOAD_LEN);
-        (self.payload.ctxt.len() - DATA_PAYLOAD_LEN) as u64
+        (self.payload.ctxt.len() - DATA_PAYLOAD_LEN)
     }
 }
 

network/src/node/broker.rs
@@ -40,7 +40,7 @@ use log::*;
 ///
 #[derive(Clone, Debug)]
 pub struct Broker {
-    upstream: mpsc::UnboundedSender<PubsubMessage>,
+    pub upstream: mpsc::UnboundedSender<PubsubMessage>,
 }
 
 impl Broker {
@@ -62,7 +62,6 @@ impl Broker {
         S: Into<String> + Clone,
     {
         let topic: String = topic.clone().into();
-        debug!("net: *Subscribed to topic '{}'*", &topic);
         let (tx, rx) = mpsc::unbounded();
         let msg = PubsubMessage::Subscribe { topic, handler: tx };
         self.upstream.unbounded_send(msg)?;
@@ -74,7 +73,6 @@ impl Broker {
         S: Into<String> + Clone,
     {
         let topic: String = topic.clone().into();
-        debug!("net: *Publishing message to topic '{}'*", &topic);
         let msg = PubsubMessage::Publish {
             topic: topic.clone().into(),
             data,
@@ -89,7 +87,7 @@ impl Broker {
 // ----------------------------------------------------------------
 
 #[derive(Clone, Debug)]
-enum PubsubMessage {
+pub enum PubsubMessage {
     Subscribe {
         topic: String,
         handler: mpsc::UnboundedSender<Vec<u8>>,
@@ -145,6 +143,7 @@ impl Future for BrokerService {
                 Ok(Async::Ready(msg)) => match msg {
                     Some(Message::Pubsub(m)) => match m {
                         PubsubMessage::Subscribe { topic, handler } => {
+                            debug!("Subscribed to topic '{}'*", &topic);
                             let new_topic = floodsub::TopicBuilder::new(topic).build();
                             let topic_hash = new_topic.hash();
                             self.consumers

node/src/bootstrap.rs
@@ -45,6 +45,14 @@ fn main() {
                 .help("Number of initial keys to generate.")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("coins")
+                .short("c")
+                .long("coins")
+                .value_name("NUMBER")
+                .help("Number of coins to create.")
+                .takes_value(true),
+        )
         .get_matches();
 
     let keys = if let Some(keys) = args.value_of("keys") {
@@ -63,6 +71,24 @@ fn main() {
         }
     } else {
         5
+    };
+
+    let coins = if let Some(coins) = args.value_of("coins") {
+        match coins.parse::<i64>() {
+            Ok(keys) => {
+                if keys < 100 {
+                    eprintln!("Invalid number of coins: must be greater than 100");
+                    process::exit(1);
+                };
+                keys
+            }
+            Err(e) => {
+                eprintln!("Invalid number of coins: {}", e);
+                process::exit(1);
+            }
+        }
+    } else {
+        1_000_000
     };
 
     info!("Generating genesis keys...");
@@ -85,15 +111,12 @@ fn main() {
     }
 
     info!("Generating genesis blocks...");
-    let (key_block, monetary_block) = genesis(&keychains);
-    let key_block_data = key_block.into_proto();
-    let key_block_data = key_block_data.write_to_bytes().unwrap();
-    let monetary_block_data = monetary_block.into_proto();
-    let monetary_block_data = monetary_block_data.write_to_bytes().unwrap();
-
-    info!("Saving genesis blocks...");
-    fs::write("genesis0.bin", &key_block_data).expect("failed to write genesis block");
-    fs::write("genesis1.bin", &monetary_block_data).expect("failed to write genesis block");
+    let blocks = genesis(&keychains, coins);
+    for (i, block) in blocks.iter().enumerate() {
+        let block_data = block.into_proto();
+        let block_data = block_data.write_to_bytes().unwrap();
+        fs::write(format!("genesis{}.bin", i), &block_data).expect("failed to write genesis block");
+    }
 
     info!("Done");
 }

commit ec3621a1be1fce112921a945f64872895097673a

Update genesis blocks
- Include KeyBlockHeader.witnesses to the block's hash
- Use `enum Block` instead of individual KeyBlock and MonetaryBlock
  structures to store genesis blocks
- Re-generate genesis blocks

Fixes a regression introduced by 40c5ad53 "Pass genesis explicitly"
Closes #57

blockchain/src/block.rs
@@ -94,8 +94,9 @@ impl Hashable for KeyBlockHeader {
     fn hash(&self, state: &mut Hasher) {
         self.base.hash(state);
         self.leader.hash(state);
-        // self.witnesses[..].hash(state);
-        // self.facilitator.hash(state);
+        for witness in self.witnesses.iter() {
+            witness.hash(state);
+        }
     }
 }
 

crypto/src/pbc/secure.rs
@@ -38,7 +38,7 @@ use rand::thread_rng;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::hash as stdhash;
-use std::ops::{Neg, Add};
+use std::ops::{Add, Neg};
 
 // --------------------------------------------------------------------------------
 
@@ -261,7 +261,6 @@ impl Add<G1> for G1 {
         add_G1_G1(&self, &other)
     }
 }
-
 
 // -----------------------------------------
 #[derive(Copy, Clone)]
@@ -560,7 +559,7 @@ impl stdhash::Hash for PublicKey {
 impl Add<PublicKey> for PublicKey {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-       PublicKey(self.0 + other.0)
+        PublicKey(self.0 + other.0)
     }
 }
 
@@ -998,4 +997,3 @@ pub fn add_G2_G2(a: &G2, b: &G2) -> G2 {
     }
     ans
 }
-

node/src/lib.rs
@@ -53,17 +53,15 @@ use tokio_timer::Interval;
 /// Load genesis blocks for tests and development.
 pub fn genesis_dev() -> Result<Vec<Block>, Error> {
     // Load generated blocks
-    let key_block = include_bytes!("../data/genesis0.bin");
-    let key_block: protos::node::KeyBlock = protobuf::parse_from_bytes(&key_block[..])?;
-    let key_block = KeyBlock::from_proto(&key_block)?;
-    let monetary_block = include_bytes!("../data/genesis1.bin");
-    let monetary_block: protos::node::MonetaryBlock =
-        protobuf::parse_from_bytes(&monetary_block[..])?;
-    let monetary_block = MonetaryBlock::from_proto(&monetary_block)?;
-    Ok(vec![
-        Block::KeyBlock(key_block),
-        Block::MonetaryBlock(monetary_block),
-    ])
+    let block1 = include_bytes!("../data/genesis0.bin");
+    let block2 = include_bytes!("../data/genesis1.bin");
+    let mut blocks = Vec::<Block>::new();
+    for block in &[&block1[..], &block2[..]] {
+        let block: protos::node::Block = protobuf::parse_from_bytes(block)?;
+        let block = Block::from_proto(&block)?;
+        blocks.push(block);
+    }
+    Ok(blocks)
 }
 
 /// Blockchain Node.

testing/node01/stegos.pkey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 PUBLIC KEY-----
-uJ3B2QNNPEKLSE/gDzmYg6r9Nk1NIutlYA3CkTDHNoM=
+CtTNWoO3KuC0LNvQ7i7rDP46OBdv52PNA8F7p3rTygc=
 -----END STEGOS-CURVE1174 PUBLIC KEY-----

testing/node01/stegos.skey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 SECRET KEY-----
-lv11h/yMpZEZYPc2KyRfn6yc6YXQDBC5/Tp0ynCOawE=
+z8EexUiuAGQn8/typR0cPl6E6YllIq4j4LDhI24vkAE=
 -----END STEGOS-CURVE1174 SECRET KEY-----

testing/node01/stegos.toml
@@ -6,12 +6,12 @@ private_key = "testing/node01/stegos.skey"
 public_key = "testing/node01/stegos.pkey"
 
 [network]
-### default = "0.0.0.0"
-# bind_ip = "127.0.0.1"
 node_id = "node01"
+bind_ip = "127.0.0.1"
 bind_port = 10055
-advetise_local_ips = false
+advertise_local_ips = false
 broadcast_topic = "chat"
+
 advertised_addresses = [
     "/ip4/127.0.0.1/tcp/10055",
 ]

testing/node02/stegos.pkey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 PUBLIC KEY-----
-fc2LT/nh1wDREHH/yG17prEQPnSR7RM73ZMX6Mih9wI=
+Rf8orMyk7DDMtYC90p1sQoJADjl9s69E8dOToiq1twc=
 -----END STEGOS-CURVE1174 PUBLIC KEY-----

testing/node02/stegos.skey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 SECRET KEY-----
-VlIfbh8ZU9BWMGbBAxzqQFGWZVinO6YgAc2e882TjwE=
+Jre6ExSYEetF3SLEe52BjX0GeS7IY4RQwscqvskF/AE=
 -----END STEGOS-CURVE1174 SECRET KEY-----

testing/node02/stegos.toml
@@ -11,9 +11,11 @@ bind_ip = "127.0.0.1"
 bind_port = 10056
 advertise_local_ips = false
 broadcast_topic = "chat"
+
 seed_nodes = [
     "/ip4/127.0.0.1/tcp/10055",
 ]
+
 advertised_addresses = [
     "/ip4/127.0.0.1/tcp/10056",
 ]

testing/node03/stegos.pkey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 PUBLIC KEY-----
-QyjXvMkq8GG3axWVFrrOD6eHWNO9bPQdf+ckVPEZfoQ=
+WTXGAY+qu4MqmDzt7JuXq5KI3Jf/8w7TdCGUmL/Z6wA=
 -----END STEGOS-CURVE1174 PUBLIC KEY-----

testing/node03/stegos.skey
@@ -1,3 +1,3 @@
 -----BEGIN STEGOS-CURVE1174 SECRET KEY-----
-ALpREy34k+Ui1kFnp00BlNN4kTFoYGnr5/nq4alaMgE=
+gDOlsNstRqSU+cUXK7ECXsQehdSrnYvd7fTxEbPV7QE=
 -----END STEGOS-CURVE1174 SECRET KEY-----

testing/node03/stegos.toml
@@ -11,10 +11,11 @@ bind_ip = "127.0.0.1"
 bind_port = 10057
 advertise_local_ips = false
 broadcast_topic = "chat"
+
 seed_nodes = [
     "/ip4/127.0.0.1/tcp/10055",
-    # "/ip4/127.0.0.1/tcp/10056",
 ]
+
 advertised_addresses = [
     "/ip4/127.0.0.1/tcp/10057",
 ]
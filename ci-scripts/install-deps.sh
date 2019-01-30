54e40e6fdfbe76859c7f2ee942585f1e9442df4f
Eugene Chupriyanov <tchu@tchu.ru>
2019-01-30 17:16:37 +0300

    Update CI to Rust 1.32 and use new library pack

--- ci-scripts/install-deps.sh
+++ ci-scripts/install-deps.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/env bash
 
 install_deps_linux() {
-  apt-get update && \
-    apt-get install -y build-essential \
-        binutils-dev \
-        clang \
-        cmake \
-        curl \
-        libcurl4-openssl-dev \
-        libdw-dev \
-        libiberty-dev \
-        libssl-dev \
-        pkg-config \
-        wget \
-        zlib1g-dev
+    apt-get update &&
+        apt-get install -y build-essential \
+            binutils-dev \
+            clang \
+            cmake \
+            curl \
+            libcurl4-openssl-dev \
+            libdw-dev \
+            libgmp-dev \
+            libmpfr-dev \
+            libiberty-dev \
+            libssl-dev \
+            pkg-config \
+            wget \
+            zlib1g-dev
 
-  (cd /usr/local && curl -L https://github.com/emotiq/emotiq-external-libs/releases/download/release-0.1.15/emotiq-external-libs-linux.tgz \
-    | tar xvfz -)
-  ldconfig
+    (cd /usr/local && curl -L https://github.com/stegos/stegos-external-libs/releases/download/v0.2/stegos-external-libs-linux.tgz |
+        tar xvfz -)
+    ldconfig
 }
 
 install_deps_macos() {
-  brew install gmp pbc protobuf zlib cmake pkg-config
+    brew install gmp mpfr pbc protobuf zlib cmake pkg-config
 }
 
 case $(uname -s) in
-    Linux*)
-        install_deps_linux
-        ;;
-    Darwin*)
-        install_deps_macos
-        ;;
-    *)
-        echo Unknown OS \"$(uname -s)\". Terminating...
-        exit 127
-        ;;
+Linux*)
+    install_deps_linux
+    ;;
+Darwin*)
+    install_deps_macos
+    ;;
+*)
+    echo Unknown OS \"$(uname -s)\". Terminating...
+    exit 127
+    ;;
 esac

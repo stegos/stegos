cc1b522f9906dd0b4774a9b51567d6c424a9c912
Vladimir Motylenko <vld@stegos.com>
2020-03-03 14:21:17 +0200

    Add api to retrive public outputs for specific account.

--- node/src/api.rs
+++ node/src/api.rs
@@ -63,6 +63,9 @@ pub enum NodeRequest {
     ReplicationInfo {},
     PopMicroBlock {},
     ChainName {},
+    PublicPaymentInfo {
+        pkey: scc::PublicKey,
+    },
     BroadcastTransaction {
         #[serde(serialize_with = "serialize_protobuf_to_hex")]
         #[serde(deserialize_with = "deserialize_protobuf_from_hex")]
@@ -121,6 +124,10 @@ pub enum NodeResponse {
     EscrowInfo(EscrowInfo),
     ReplicationInfo(ReplicationInfo),
     MicroBlockPopped,
+    PublicPaymentInfo {
+        #[serde(flatten)]
+        info: stegos_blockchain::api::PublicOutputsInfo,
+    },
     ChainName {
         name: String,
     },

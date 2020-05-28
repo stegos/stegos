3e02c117d38e1c2772781741ba41e489ecf6e91c
Joel Reymont <joelr1@gmail.com>
2020-05-28 17:20:03 +0300

    Remove size limit on the client websocket frame

--- api/src/client.rs
+++ api/src/client.rs
@@ -31,6 +31,7 @@ use futures::prelude::*;
 use futures::SinkExt;
 use log::*;
 use tokio::net::TcpStream;
+use tungstenite::protocol::WebSocketConfig;
 use tokio_tungstenite::tungstenite::Error as WsError;
 use tokio_tungstenite::tungstenite::Message;
 use tokio_tungstenite::WebSocketStream;
@@ -58,7 +59,8 @@ pub struct WebSocketClient {
 
 impl WebSocketClient {
     pub async fn new(endpoint: String, api_token: ApiToken) -> Result<Self, Error> {
-        let connection = tokio_tungstenite::connect_async(&endpoint).await?.0;
+        let config = WebSocketConfig { max_frame_size: None, ..Default::default() };
+        let connection = tokio_tungstenite::connect_async_with_config(&endpoint, Some(config)).await?.0;
         let pending_notifications = VecDeque::new();
         Ok(Self {
             endpoint,

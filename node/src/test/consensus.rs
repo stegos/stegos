751c8267c54876ce1cd38cfff1577ff4737088d3
Joel Reymont <joelr1@gmail.com>
2020-06-26 10:03:35 +0300

    Start the timers running

--- node/src/test/consensus.rs
+++ node/src/test/consensus.rs
@@ -49,6 +49,8 @@ async fn smoke_test() {
     let mut sb = Sandbox::new(config.clone());
     let mut p = sb.partition();
 
+    wait(config.node.micro_block_timeout).await;
+
     trace!(">>> Iterating through {} macroblocks, with {} microblocks each", 
         1 + NUM_RESTAKES * config.chain.stake_epochs + 1,
         config.chain.micro_blocks_in_epoch
